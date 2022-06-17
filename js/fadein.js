87729e42324af5095955cc10bdd7e10c6162a651
daisuke33 <81845217+daisuke33@users.noreply.github.com>
2022-06-18 00:29:40 +0900

    brush_up_17-JUN

--- js/fadein.js
+++ js/fadein.js
@@ -11,39 +11,47 @@ $(function() {
 });
 
 // ============== 各セクション ふわっと下から
+// リロード時、スクロール前でもアクション実行されるように
+fadeInDisplay();
+
+function fadeInDisplay() {
+  const windowHeight = $(window).height();
+  const scroll = $(window).scrollTop();
+  $('.under-fadein').each(function () {
+    const targetPosition = $(this).offset().top;
+    if (scroll > targetPosition - windowHeight + 100) {
+      $(this).addClass("add-fadein");
+    }
+  });
+}
+
+
 $(window).scroll(function () {
-	const windowHeight = $(window).height();
-	const scroll = $(window).scrollTop();
-	$('.under-fadein').each(function () {
-		const targetPosition = $(this).offset().top;
-		if (scroll > targetPosition - windowHeight + 100) {
-			$(this).addClass("is-fadein");
-		}
-	});
+  fadeInDisplay();
 });
 
 
 // =============== aboutセクション 浮き出る表示のアクション ===============
 // リロード時、スクロール前でもアクション実行されるように
-aboutFadeInDisplay ();
+// aboutFadeInDisplay ();
 
-$(window).scroll(function () {
-  aboutFadeInDisplay ();
-});
-// 関数化
-function aboutFadeInDisplay () {
-  const windowHeight = $(window).height();
-  const scroll = $(window).scrollTop();
-  $('.js-img').each(function () {
-    const targetPosition = $(this).offset().top;
-    if (scroll > targetPosition - windowHeight + 100) {
-      $(this).addClass("is-fadein");
-    }
-  });
-  $('.js-author').each(function () {
-    const targetPosition = $(this).offset().top;
-    if (scroll > targetPosition - windowHeight + 100) {
-      $(this).addClass("is-fadein");
-    }
-  });
-}
+// $(window).scroll(function () {
+//   aboutFadeInDisplay ();
+// });
+// // 関数化
+// function aboutFadeInDisplay () {
+//   const windowHeight = $(window).height();
+//   const scroll = $(window).scrollTop();
+//   $('.js-img').each(function () {
+//     const targetPosition = $(this).offset().top;
+//     if (scroll > targetPosition - windowHeight + 100) {
+//       $(this).addClass("is-fadein");
+//     }
+//   });
+//   $('.js-author').each(function () {
+//     const targetPosition = $(this).offset().top;
+//     if (scroll > targetPosition - windowHeight + 100) {
+//       $(this).addClass("is-fadein");
+//     }
+//   });
+// }
