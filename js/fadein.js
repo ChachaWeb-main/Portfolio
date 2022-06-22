0b8e29e57c450eb07143f11ed63ebb26045da3f5
daisuke33 <81845217+daisuke33@users.noreply.github.com>
2022-06-23 00:21:15 +0900

    brush_up_22-JUN

--- js/fadein.js
+++ js/fadein.js
@@ -1,16 +1,37 @@
 'use strict';
 
-// ============== サイトサクセス時のアニメーション
-$(function() {
-	setTimeout(function(){
-		$('.fadeIn img').fadeIn(1000);
-	},200);
-	setTimeout(function(){
-		$('.fadeIn').fadeOut(1000);
-	},3000);
+// ============== サイトサクセス時のアニメーション ===============
+// ライブラリを使用（ProgressBar.js というローディング時の数字のカウントアップとプログレスバーを表示）
+var bar = new ProgressBar.Line(splash_text, {
+  easing: "easeInOut", //アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+  duration: 1500, //時間指定(1000＝1秒)
+  strokeWidth: 0.2, //進捗ゲージの太さ
+  color: "#555", //進捗ゲージのカラー
+  trailWidth: 0.2, //ゲージベースの線の太さ
+  trailColor: "#bbb", //ゲージベースの線のカラー
+  text: {
+    style: {
+      //中央配置
+      position: "absolute",
+      left: "50%",
+      top: "50%",
+      padding: "0",
+      margin: "-30px 0 0 0", //バーより上に配置
+      transform: "translate(-50%,-50%)", "font-size": "2rem",
+      color: "#fff"
+    },
+    autoStyleContainer: false //自動付与のスタイルを切る
+  },
+  step: function (state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + " %"); //テキストの数値
+  }
+});
+// アニメーションスタート
+bar.animate(1.0, function () { // バーを描画する割合を指定します 1.0 なら100%まで描画します
+  $("#splash").delay(1000).fadeOut(1200); //アニメーションが終わったら#splashエリアをフェードアウト
 });
 
-// ============== 各セクション ふわっと下から
+// ============== 各セクション ふわっと下から ===============
 function fadeInDisplay() {
   const windowHeight = $(window).height();
   const scroll = $(window).scrollTop();
@@ -21,10 +42,8 @@ function fadeInDisplay() {
     }
   });
 }
-
 // リロード時、スクロール前でもアクション実行されるように
 fadeInDisplay();
-
 $(window).scroll(function () {
   fadeInDisplay();
 });
