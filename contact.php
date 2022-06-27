7b3afd1f8fab85bd56dbbb9269737fbca74e396f
daisuke33 <81845217+daisuke33@users.noreply.github.com>
2022-06-27 23:07:27 +0900

    brushUp-01_contactForm

--- contact.php
+++ contact.php
@@ -1,42 +1,50 @@
 <?php
 session_start();
-// 電話番号ハイフンあり正規表現
-$option = '/^0[0-9]{1,4}-[0-9]{1,4}-[0-9]{3,4}\z/';
+
 // エラーメッセージ判定
 $error = [];
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $post = filter_input_array(INPUT_POST, $_POST);
-    // フォーム送信時にエラーチェック
-    if (empty($post['name'])) {
-        $error['name'] = 'blank';
-    } elseif (mb_strlen($post['name']) > 50) {
-        $error['name'] = 'name';
-    }
+  $post = filter_input_array(INPUT_POST, $_POST);
+  // フォーム送信時にエラーチェック
+  if (empty($post['name'])) {
+    // 空の場合
+    $error['name'] = 'blank';
+  } elseif (preg_match("/^[ぁ-んァ-ヶ一ー-龠a-zA-Z0-9]{1,50}+$/u", $post['name'])) {
+    // 正規表現に合致する場合(ひらがな・漢字・アルファベット, 1 ~ 50字)
+  } else {
+    // 正規表現に合致しない場合
+    $error['name'] = 'invaild';
+  }
 
-    if (empty($post['email'])) {
-        $error['email'] = 'blank';
-    } elseif (!filter_var($post['email'], FILTER_VALIDATE_EMAIL)) {
-        $error['email'] = 'email';
-    }
+  if (empty($post['email'])) {
+    // 空の場合
+    $error['email'] = 'blank';
+  } elseif (!filter_var($post['email'], FILTER_VALIDATE_EMAIL)) {
+    $error['email'] = 'invaild';
+  }
 
-    if (!filter_var($post['phone'], FILTER_VALIDATE_REGEXP, $option)) {
-        $error['phone'] = 'phone';
-    }
+  if (preg_match("/^0[-0-9]{9,13}$/", $post['phone'])) {
+    // 正規表現に合致する場合
+  } else {
+    // 正規表現に合致しない場合
+    $error['phone'] = 'invaild';
+  }
 
-    if (empty($post['message'])) {
-        $error['message'] = 'blank';
-    }
+  if (empty($post['message'])) {
+    // 空の場合
+    $error['message'] = 'blank';
+  }
 
-    if (count($error) === 0) {
-        // エラーがないので確認画面に移動
-        $_SESSION['form'] = $post;
-        header('Location: confirm.php');
-        exit();
-    }
+  if (count($error) === 0) {
+    // エラーがないので確認画面に移動
+    $_SESSION['form'] = $post;
+    header('Location: confirm.php');
+    exit();
+  }
 } else {
-    if (isset($_SESSION['form'])) {
-        $post = $_SESSION['form'];
-    }
+  if (isset($_SESSION['form'])) {
+    $post = $_SESSION['form'];
+  }
 }
 
 echo '代入値の確認';
@@ -64,8 +72,7 @@ echo '</pre>';
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Pacifico&amp;display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Sansita+Swashed:wght@600&display=swap"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Sansita+Swashed:wght@600&display=swap" rel="stylesheet">
   <!-- CSS -->
   <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
   <link rel="stylesheet" href="./css/style.css">
@@ -109,62 +116,72 @@ echo '</pre>';
       <form action="" method="post" novalidate>
         <p class="contact-info">お問い合わせ内容をご入力の上、「確認画面へ」をクリックしてください。</p>
         <table class="contact-table">
-            <tr>
-                <th>お名前<span class="required">必須</span></th>
-                <td>
-                    <input size="20" type="text" class="wide" name="name" placeholder="ex).  山田太郎" value="<?php echo htmlspecialchars($post['name']); ?>"/>
-                    <?php if($error['name'] === 'blank') : ?>
-                        <p class="error_msg">※お名前をご記入下さい</p>
-                    <?php endif; ?>
-                    <?php if($error['name'] === 'name') : ?>
-                        <p class="error_msg">※お名前は50字以内で入力して下さい</p>
-                    <?php endif; ?>
-                </td>
-            </tr>
-            <tr>
-                <th>メールアドレス<span class="required">必須</span></th>
-                <td>
-                    <input size="30" type="text" class="wide" name="email" placeholder="ex).  example@gmail.com    ※半角英数" value="<?php echo htmlspecialchars($post['email']); ?>" required/>
-                    <?php if($error['email'] === 'blank') : ?>
-                        <p class="error_msg">※メールアドレスをご記入下さい</p>
-                    <?php endif; ?>
-                    <?php if($error['email'] === 'email') : ?>
-                        <p class="error_msg">※メールアドレスが不正です</p>
-                    <?php endif; ?>
-                </td>
-            </tr>
-            <tr>
-                <th>電話番号<span class="any">任意</sapn></th>
-                <td>
-                    <input size="30" type="text" class="wide" name="phone" placeholder="ex).  012-3456-7890   ※半角数字" value="<?php echo htmlspecialchars($post['phone']); ?>"/>
-                    <?php if($error['phone'] === 'phone') : ?>
-                        <p class="error_msg">※電話番号は半角数字をご記入下さい</p>
-                    <?php endif; ?>
-                </td>
-            </tr>
-            <tr>
-                <th>お問い合わせ内容<span class="required">必須</span></th>
-                <td>
-                    <textarea name="message" cols="50" rows="5" placeholder="お見積もりは無料で承ります。まずはお気軽にお問い合わせくださいませ。" required><?php echo htmlspecialchars($post['message']); ?></textarea>
-                    <?php if($error['message'] === 'blank') : ?>
-                        <p class="error_msg">※お問い合わせ内容をご記入下さい</p>
-                    <?php endif; ?>
-                </td>
-            </tr>
+          <tr>
+            <th>お名前<span class="required">必須</span></th>
+            <td>
+              <input size="20" type="text" class="wide" name="name" placeholder="ex).  山田太郎" value="<?php echo htmlspecialchars($post['name']); ?>" />
+              <!-- Notice: Undefined index の防止 -->
+              <?php if (!empty($error['name'])) : ?>
+                <?php if ($error['name'] === 'blank') : ?>
+                  <p class="error_msg">※お名前をご記入下さい</p>
+                <?php endif; ?>
+                <?php if ($error['name'] === 'invaild') : ?>
+                    <p class="error_msg">※お名前は50字以内で入力して下さい</p>
+                <?php endif; ?>
+              <?php endif; ?>
+            </td>
+          </tr>
+          <tr>
+            <th>メールアドレス<span class="required">必須</span></th>
+            <td>
+              <input size="30" type="text" class="wide" name="email" placeholder="ex).  example@gmail.com    ※半角英数" value="<?php echo htmlspecialchars($post['email']); ?>" required />
+              <?php if (!empty($error['email'])) : ?>
+                <?php if ($error['email'] === 'blank') : ?>
+                  <p class="error_msg">※メールアドレスをご記入下さい</p>
+                <?php endif; ?>
+                <?php if ($error['email'] === 'invaild') : ?>
+                  <p class="error_msg">※メールアドレスが不正です</p>
+                <?php endif; ?>
+              <?php endif; ?>
+            </td>
+          </tr>
+          <tr>
+            <th>電話番号<span class="any">任意</sapn>
+            </th>
+            <td>
+              <input size="30" type="text" class="wide" name="phone" placeholder="ex).  012-3456-7890   ※半角数字" value="<?php echo htmlspecialchars($post['phone']); ?>" />
+              <?php if (!empty($error['phone'])) : ?>
+                <?php if ($error['phone'] === 'invaild') : ?>
+                  <p class="error_msg">※電話番号は半角数字をご記入下さい</p>
+                <?php endif; ?>
+              <?php endif; ?>
+            </td>
+          </tr>
+          <tr>
+            <th>お問い合わせ内容<span class="required">必須</span></th>
+            <td>
+              <textarea name="message" cols="50" rows="5" placeholder="お見積もりは無料で承ります。まずはお気軽にお問い合わせくださいませ。" required><?php echo htmlspecialchars($post['message']); ?></textarea>
+              <?php if (!empty($error['message'])) : ?>
+                <?php if ($error['message'] === 'blank') : ?>
+                  <p class="error_msg">※お問い合わせ内容をご記入下さい</p>
+                <?php endif; ?>
+              <?php endif; ?>
+            </td>
+          </tr>
         </table>
 
         <div class="box_check">
-            <label>
-                <input type="checkbox" name="acceptance-714" value="1" aria-invalid="false" class="agree"><span class="check">プライバシーポリシーに同意する</span>
-            </label>
+          <label>
+            <input type="checkbox" name="acceptance-714" value="1" aria-invalid="false" class="agree"><span class="check">プライバシーポリシーに同意する</span>
+          </label>
         </div>
         <p class="confirm-btn">
-            <span><input type="submit" name="confirm" value="確認画面へ" /></span>
+          <span><input type="submit" name="confirm" value="確認画面へ" /></span>
         </p>
         <br>
         <br>
 
-    </form>
+      </form>
     </div>
     <br>
     <br>
@@ -177,8 +194,7 @@ echo '</pre>';
   </footer>
 
   <!-- jQuery -->
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <!-- JavaScript -->
   <script src="./js/fadein.js"></script>
   <script src="./js/main.js"></script>
