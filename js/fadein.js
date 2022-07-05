25a40a87bd48406f670947cb4a77141ed784f927
daisuke33 <81845217+daisuke33@users.noreply.github.com>
2022-07-06 08:26:44 +0900

    brushUp_06-July

--- js/fadein.js
+++ js/fadein.js
@@ -5,10 +5,10 @@
 function fadeInDisplay() {
   const windowHeight = $(window).height();
   const scroll = $(window).scrollTop();
-  $('.under-fadein').each(function () {
+  $('.fadein-under').each(function () {
     const targetPosition = $(this).offset().top;
     if (scroll > targetPosition - windowHeight + 100) {
-      $(this).addClass("add-fadein");
+      $(this).addClass("fadein-add");
     }
   });
 }
@@ -20,29 +20,3 @@ $(function() {
     fadeInDisplay();
   });
 });
-
-
-// =============== aboutセクション 浮き出る表示のアクション =============== 削除予定！！！
-// リロード時、スクロール前でもアクション実行されるように
-// aboutFadeInDisplay ();
-
-// $(window).scroll(function () {
-//   aboutFadeInDisplay ();
-// });
-// // 関数化
-// function aboutFadeInDisplay () {
-//   const windowHeight = $(window).height();
-//   const scroll = $(window).scrollTop();
-//   $('.js-img').each(function () {
-//     const targetPosition = $(this).offset().top;
-//     if (scroll > targetPosition - windowHeight + 100) {
-//       $(this).addClass("is-fadein");
-//     }
-//   });
-//   $('.js-author').each(function () {
-//     const targetPosition = $(this).offset().top;
-//     if (scroll > targetPosition - windowHeight + 100) {
-//       $(this).addClass("is-fadein");
-//     }
-//   });
-// }
